b03225f91514259d351b5a26dd06c89ff0d2b16b
Rishab 🤓 <rissaraf@amazon.com>
2018-11-19 16:36:44 -0800

    Send redirect response from server or render page not found when id is present

--- src/main/java/com/rishab/shortly/controller/HomePageController.java
+++ src/main/java/com/rishab/shortly/controller/HomePageController.java
@@ -1,16 +1,44 @@
 package com.rishab.shortly.controller;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.rishab.shortly.pojo.UrlDto;
+import com.rishab.shortly.repo.UrlRepository;
+import com.rishab.shortly.util.UrlIdConversionUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class HomePageController {
+
+    private final UrlRepository urlRepository;
+
+    @Autowired
+    public HomePageController(final UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public String homePage() {
+    public String homePage(@PathVariable final Optional<String> id, final HttpServletResponse response) {
+        if (id.isPresent()) {
+            try {
+                UrlDto urlDto = urlRepository.findById(UrlIdConversionUtil.generate(id.get()))
+                                             .get();
+                response.sendRedirect(urlDto.getUrl());
+            } catch (Exception e) {
+                log.error("Failed", e);
+                return "pageNotFound";
+            }
+        }
         return "index";
     }
 }
--- src/main/resources/static/js/shortly.js
+++ src/main/resources/static/js/shortly.js
@@ -4,25 +4,6 @@ var Shortly = angular.module('Shortly', []).controller('ShortlyController', ['$s
     $scope.error = "";
     $scope.shortUrl = "";
     $scope.requestUrl = "https://";
-    $scope.isHome = false;
-
-    $scope.initHome = function () {
-        if (!!$window.location.pathname && $window.location.pathname.trim() != "/") {
-            $scope.isHome = false;
-            $http({method: "GET", url: "/rest" + $window.location.pathname}).then(function (response) {
-                $scope.hasError = response.data.hasError;
-                $scope.error = response.data.errorMessage;
-                if (!$scope.error && response.data.data && response.data.data.url) {
-                    $window.location.href = response.data.data.url;
-                }
-            }, function (response) {
-                $scope.hasError = true;
-                $scope.error = 'Something went wrong!';
-            });
-        } else {
-            $scope.isHome = true;
-        }
-    };
 
     $scope.shorten = function (requestUrl) {
         var requestData = {
